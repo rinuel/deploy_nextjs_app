81e549638c90feb0083350d32085999c931e70a6
JeonKwangYoung <rinuel@naver.com>
2024-04-03 15:41:04 +0900

    Update page.tsx
    sdaf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
       </div>
       <h2 className={inter.className}>
-           Welcome to 5minslearn!
+           Welcome to 5minslearn!@#@#@##
           </h2>
 
       <div className={styles.center}>
