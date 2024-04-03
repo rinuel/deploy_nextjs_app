4e66484de0ef3305c0acf46313a4e7d6ca0e18db
JeonKwangYoung <rinuel@naver.com>
2024-04-03 15:19:59 +0900

    Update deploy.sh
    --

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 export NVM_DIR=$HOME/.nvm;
 . $NVM_DIR/nvm.sh;
 
-cd /home/ubuntu/deploy_nextjs_app
+cd /home/ec2-user/deploy_nextjs_app
 git pull origin master
-nvm use v16
+nvm use 20
 yarn install &&
 yarn build &&
 pm2 restart next_hello_world_app
